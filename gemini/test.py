21e75b48b11503831a9fd94607117f7cea7ccb01
wei-sjtu <tobellw169@sjtu.edu.cn>
2025-09-29 16:31:19 +0800

    fix secret leakage in test.py

--- gemini/test.py
+++ gemini/test.py
@@ -1,6 +1,6 @@
 from google import genai
 from google.genai import types
-client = genai.Client(api_key="AIzaSyA0-9IM_Do60VCfinrJieTXjgyZFLTxNIs")
+client = genai.Client(api_key="dfafafaffafdaf")
 
 response = client.models.generate_content(
     model="gemini-2.5-pro", contents="please introduce yourself"
